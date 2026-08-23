0f114944b4e2e6646d2941f983b1d8b02b8975c3
ewhalek <ewhale2d@gmail.com>
2026-08-23 20:07:16 +0630

    Add banner and popup ad scripts to ads.js

---
+++ ads.js
@@ -0,0 +1,18 @@
+document.write(`
+    <!-- Banner Ad -->
+    <div style="margin: 20px 0; min-height: 250px;">
+        <script type="text/javascript">
+            atOptions = {
+                'key' : 'ef112de2e0e8a6461af139c51bacf4cc',
+                'format' : 'iframe',
+                'height' : 50,
+                'width' : 320,
+                'params' : {}
+            };
+        </script>
+        <script type="text/javascript" src="https://www.highrevenueformat.com/ef112de2e0e8a6461af139c51bacf4cc/invoke.js"></script>
+    </div>
+
+    <!-- Popup Ad -->
+    <script type="text/javascript" src="https://pl30991713.profitableratecpmnetwork.com/93/e4/72/93e4727fb85e5496304aa91e0db919a6.js"></script>
+`);
