65fb425be3202e33424c71783339221524a11639
ewhalek <ewhale2d@gmail.com>
2026-08-23 20:13:52 +0630

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,18 +1,11 @@
-document.write(`
-    <!-- Banner Ad -->
-    <div style="margin: 20px 0; min-height: 250px;">
+document.getElementById('ad-container').innerHTML = `
+    <div style="margin-bottom: 20px;">
         <script type="text/javascript">
-            atOptions = {
-                'key' : 'ef112de2e0e8a6461af139c51bacf4cc',
-                'format' : 'iframe',
-                'height' : 50,
-                'width' : 320,
-                'params' : {}
-            };
+            atOptions = { 'key' : 'ef112de2e0e8a6461af139c51bacf4cc', 'format' : 'iframe', 'height' : 50, 'width' : 320, 'params' : {} };
         </script>
         <script type="text/javascript" src="https://www.highrevenueformat.com/ef112de2e0e8a6461af139c51bacf4cc/invoke.js"></script>
     </div>
-
-    <!-- Popup Ad -->
-    <script type="text/javascript" src="https://pl30991713.profitableratecpmnetwork.com/93/e4/72/93e4727fb85e5496304aa91e0db919a6.js"></script>
-`);
+`;
+var script = document.createElement('script');
+script.src = "https://pl30991713.profitableratecpmnetwork.com/93/e4/72/93e4727fb85e5496304aa91e0db919a6.js";
+document.body.appendChild(script);
